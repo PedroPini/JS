314a36dcc508c316a968703425bc7b0bbd30ff82
PedroPini <augustopini@gmail.com>
2019-08-25 16:42:56 -0300

    Removendo select criado para fins didaticos

--- DatingApp/DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,17 +7,21 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration sucessful');
+    }, error => {
+      console.log(error);
+    });
+    // console.log(this.model);
   }
 
   cancel() {
