19b9e0f6a60186c3bc29359be0c8c08c5a11a94f
PedroPini <augustopini@gmail.com>
2019-09-19 23:32:31 -0300

    Adicionando tratamento na pagina de edicao

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 export function tokenGetter() {
@@ -83,7 +84,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
-      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
+      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig},
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
